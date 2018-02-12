953737676c91a86604c7da6fee5f233e806af166
somkiat <somkiat.p@gmail.com>
2018-02-12 15:00:45 +0700

    Learn KTX

--- app/src/main/java/cc/somkiat/demokotlin/MainActivity.kt
+++ app/src/main/java/cc/somkiat/demokotlin/MainActivity.kt
@@ -1,13 +1,37 @@
 package cc.somkiat.demokotlin
 
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.content.edit
+import androidx.net.toUri
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Kotlin
+        val uri = Uri.parse("Hello")
+        // KTX
+        val uri2 = "Hello".toUri();
+
+        val sharedPreferences = this.applicationContext.getSharedPreferences(this)
+
+        //Kotlin
+        sharedPreferences.edit()
+                .putBoolean("key", true)
+                .putBoolean("key2", true)
+                .putString("key3", "Hello")
+                .apply()
+        //KTX
+        sharedPreferences.edit {
+            putBoolean("key1", true)
+            putBoolean("key2", true)
+            putString("key3", "Hello")
+        }
+
     }
 }
 
